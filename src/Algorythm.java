116573a92d079acf0647e1f50db27083f80c6d55
Anastasia30711405 <56717134+Anastasia30711405@users.noreply.github.com>
2020-01-12 23:56:35 +0600

    Create Algorythm.java

---
+++ src/Algorythm.java
@@ -0,0 +1,12 @@
+class Algorythm {
+    private String msg;
+    public Algorythm(){
+        msg = "Hello World!";
+    }
+    public Algorythm(String msg){
+        this.msg = msg;
+    }
+    public  void out() {
+        System.out.println(msg);
+    }
+}
